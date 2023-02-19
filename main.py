955e9d37a2a49a738b299a95e3f44f0c473d411e
Edijs Ilvess 221RDB226 <edijs.ilvess@edu.rtu.lv>
2023-02-19 21:37:34 +0200

    .

--- main.py
+++ main.py
@@ -14,18 +14,27 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, i+1))
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
+            if len(opening_brackets_stack)==0 or not are_matching(opening_brackets_stack[-1].char,next):
+                return i+1
+            opening_brackets_stack.pop()
             pass
+    return "Success"
 
 
 def main():
-    text = input()
+    burtu_text = input()
+    if (burtu_text[0] == "F" or burtu_text[0] == "I"):
+        text = input()
+    else:
+        return
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-
+    print(mismatch)
 
 if __name__ == "__main__":
     main()
